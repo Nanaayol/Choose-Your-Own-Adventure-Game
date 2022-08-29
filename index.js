f057328dda47c4295897be35a57378a28a0cdab1
Nana Ayoluwa <akuabamidele@Nanas-MacBook-Pro.local>
2022-08-29 15:15:54 -0400

    realized these switch/else ifs are to be nested within each other.B/C assigns variables locally. work within their own paths.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 let answer = window.prompt("Do you head left or right?")
 console.log (answer === "right")
-var thirdAnswer
-var fourthAnswer
+
 
 if(answer === "left"){
     let secondAnswer = window.prompt(`You come across a stray cat.
@@ -13,10 +12,22 @@ if(answer === "left"){
     
     switch (secondAnswer){ 
         case "follow": 
-         window.prompt (`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
+         let thirdAnswerfollow = window.prompt (`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
+            if (thirdAnswerfollow === "stay"){
+                window.prompt (`You live happily amongst the cats for the rest of your days.`)
+            }
+            else if (thirdAnswerfollow === "spread the word"){
+                window.prompt (`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
+            }
     break
         case "continue": 
-         window.prompt (`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
+         let thirdAnswercontinue = window.prompt (`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
+            if (thirdAnswercontinue === "ladder"){
+                window.prompt (`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)    
+            }
+            else if (thirdAnswercontinue === "staircase"){
+                window.prompt (`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
+            }
     break
 }}
  
@@ -28,37 +39,23 @@ else if(answer === "right") {
 
     switch (secondAnswer){
         case "past the dragon": 
-     window.prompt ('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+        let thirdAnswerPD = window.prompt ('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
+            if (thirdAnswerPD === "stay") {
+            window.prompt (`You and the dragon have an uplifting conversation about local politics and become lifelong friends.`)
+            }
+            else if (thirdAnswerPD === "run"){
+            window.prompt (`Quickly, you run back to the cave's entrance. Sheepish, you return home.`)
+            }
     break
         case "away from the dragon": 
-     window.prompt(`After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`)
+        let thirdAnswerAD = window.prompt(`After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`)
+            if (thirdAnswerAD === "draw it"){
+            window.prompt (`You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.`)
+            }
+            else if (thirdAnswerAD === "pick it"){
+            window.prompt (`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`)
+            }
+    break
 }}
 
-
-switch (thirdAnswer){
-    case "stay":
-    let fourthAnswer = window.prompt (`You live happily amongst the cats for the rest of your days.`)
-break
-    case "spread the word":
-    window.prompt (`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
-break
-    case "ladder":
-    window.prompt (`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)
-break
-    case "staircase":
-    window.prompt (`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
-break
-    case "stay":
-    window.prompt (`You and the dragon have an uplifting conversation about local politics and become lifelong friends.`)
-break 
-    case "run":
-    window.prompt (`Quickly, you run back to the cave's entrance. Sheepish, you return home.`)
-break
-    case "draw it":
-    window.prompt (`You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.`)
-break
-    case "pick it":
-    window.prompt (`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`)
-break
-}}}
 
